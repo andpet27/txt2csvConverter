72b59024d22fcc1b33ea4dc23eb7233955b1d286
Andrej <peternelandrej0@gmail.com>
2020-03-24 15:25:40 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from tkinter import Tk
+from tkinter import simpledialog
+from tkinter import filedialog
+from tkinter import *
+
+Tk().withdraw()
+fileName = filedialog.askopenfile(initialdir="/home", filetypes=[("Text files", "*.txt")])
+fileToConvert = pd.read_fwf(fileName)
+fileInput = simpledialog.askstring(title="CSV file name", prompt="Enter CSV file name")
+newFile = "%s.csv" %fileInput
+fileToConvert.to_csv(newFile)
